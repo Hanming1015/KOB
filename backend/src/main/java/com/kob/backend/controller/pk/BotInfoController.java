dc18f40e70296288f3e950eb86db484b3e43a931
Hanming <1983161378@qq.com>
2026-02-04 12:06:26 +0000

    skeloton of web & backend

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -10,16 +10,10 @@ import java.util.*;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getBotInfo(){
-        List<Map<String, String>> list = new LinkedList<>();
+    public Map<String, String> getBotInfo(){
         Map<String, String> bot1 = new HashMap<>();
         bot1.put("name", "tiger");
         bot1.put("rating", "5000");
-        Map<String, String> bot2 = new HashMap<>();
-        bot2.put("name", "lion");
-        bot2.put("rating", "8000");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+        return bot1;
     }
 }
