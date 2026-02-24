6e0586f42eeb4522367a743206b262a9279a3f84
Hanming <1983161378@qq.com>
2026-02-24 20:23:53 +0000

    spring security

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -1,13 +1,18 @@
 package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     void contextLoads() {
+        // 打印出加密后的字符串
+        System.out.println(passwordEncoder.encode("123456"));
     }
-
 }
